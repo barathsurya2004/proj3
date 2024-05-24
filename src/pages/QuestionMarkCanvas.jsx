342a5f196d73fd7e5055a8c42d1fcc94ce823bf2
Zeus Crusher <barathsurya2004@gmail.com>
2024-05-24 21:36:30 +0530

    done with qmark animation

--- src/pages/QuestionMarkCanvas.jsx
+++ src/pages/QuestionMarkCanvas.jsx
@@ -5,6 +5,8 @@ const QuestionMarkCanvas = () => {
   return (
     <div className="q-mark canvas">
       <Canvas>
+        <ambientLight intensity={0.7} />
+        <pointLight intensity={20} color={"#FFEDDE"} position={[5, 3, 3]} />
         <QMark />
       </Canvas>
     </div>
